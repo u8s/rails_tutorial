fbd6a48d843d244dbd601b6d1ac1d858c7a3af6e
u8s <u8sato@yahoo.co.jp>
2025-09-23 12:20:17 +0000

    Impliment basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get  "/help",    to: "static_pages#help"
-  get  "/about",   to: "static_pages#about"
-  get  "/contact", to: "static_pages#contact"
-  get  "/sign_up", to: "users#new"
+  root   "static_pages#home"
+  get    "/help",    to: "static_pages#help"
+  get    "/about",   to: "static_pages#about"
+  get    "/contact", to: "static_pages#contact"
+  get    "/sign_up", to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
